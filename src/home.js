f10ee2b49902787f80440844695bb8da21899a32
Sami Siddiqui <samisiddiqui@airmail.cc>
2022-07-03 12:55:08 -0500

    Added backgrounds

--- src/home.js
+++ src/home.js
@@ -1,7 +1,11 @@
 function home() {
     const content = document.getElementById('content');
-    content.setAttribute('style', 'color: white');
+    content.setAttribute('style', 'color: #1E3FAE');
     content.innerHTML = `<p>World-class food, world-class atmosphere</p>`;
+    document.body.style.backgroundImage = "url('99c7687a8f053aef7142.jpg')";
+    document.body.style.backgroundSize = '';
+    document.body.style.backgroundRepeat = '';
+    document.body.style.backgroundPosition = '';
 };
 
 export default home;
--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,10 @@ function menu() {
     const content = document.getElementById('content');
     content.setAttribute('style', 'color: #FF69B4');
     content.innerHTML = `<p>Food Menu</p>`;
+    document.body.style.backgroundImage = "url('57e9ade8e54e0025ab0a.jpg')";
+    document.body.style.backgroundSize = '';
+    document.body.style.backgroundRepeat = '';
+    document.body.style.backgroundPosition = '';
 };
 
 export default menu;
